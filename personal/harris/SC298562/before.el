bd004c6d7b761d14570059f51ad046f4e4825f13
unknown <cbrown33@.gs.myharris.net>
2008-12-12 16:44:30 -0500

    added gnuwin and msys bins to the path

--- personal/harris/SC298562/before.el
+++ personal/harris/SC298562/before.el
@@ -4,3 +4,9 @@
 
 ;; AIP-IRAD requires a tab-width of 4
 ;(setq kooru/tab-width 4)
+
+;; Add gnuwin utils to PATH.
+;; This is so diff and find can work.
+(setenv "PATH" (concat "C:/util/gnuwin/bin;"
+                       "C:/msys/1.0/bin;"
+                       (getenv "PATH")))
