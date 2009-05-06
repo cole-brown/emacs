d45a6cdffad3ba24537317b62482423b38367092
Cole Brown <git@spydez.com>
2009-05-06 01:04:22 -0400

    fixed drag and drop for OS X

--- personal/kooru/aluminum/after.el
+++ personal/kooru/aluminum/after.el
@@ -14,6 +14,13 @@
 ;; mouse scroll doesn't work by default in OS X.5 for some reason...
 (mwheel-install)
 
+;; this mapping opens the file dropped on an emacs window as opposed
+;; to inserting its contents at the current cursor position
+(define-key global-map [ns-drag-file] 'ns-find-file)
+
+;; if this isn't set to nil, the dragged file will open in a new frame
+(setq ns-pop-up-frames nil)
+
 ;; bell ringing sucks, but may be wanted occasionally...
 (setq ring-bell-function 
   (lambda ()
