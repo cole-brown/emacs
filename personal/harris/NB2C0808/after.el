dea0802d61a70e356fe770fb750f8b839a8802ed
Cole Brown <git@spydez.com>
2012-03-19 16:30:45 -0500

    New work laptop config files for new work laptop.

---
+++ personal/harris/NB2C0808/after.el
@@ -0,0 +1,10 @@
+
+;; some setup for org-agenda
+(setq org-agenda-files (list "c:/home/documents/todo/work.org"))
+
+;; etags support
+;;(defvar harris/denali.tags "h:/etags/denali.TAGS" "*Path to tags for Denali baseline.")
+
+;;(require 'etags)
+;;(setq tags-table-list `(,harris/denali.tags))
+
---
+++ personal/harris/NB2C0808/before.el
@@ -0,0 +1,14 @@
+
+;; Denali program requires a tab-width of 3
+(setq kooru/tab-width 3)
+(setq kooru/fill-column 80)
+
+;; AIP-IRAD requires a tab-width of 4
+;(setq kooru/tab-width 4)
+
+;; Add gnuwin utils to PATH.
+;; This is so diff and find can work.
+;;(setenv "PATH" (concat "C:/util/gnuwin/bin;"
+;;                       "C:/msys/1.0/bin;"
+;;                       (getenv "PATH")))
+;;
